d679e2075c4b021d4e19213f781ca1d6acf112b9
SELVENDRANE K <selvendrane2005@gmail.com>
2026-06-02 10:55:51 +0530

    Update Main.java

--- Core JAVA/StudentDAO/Main.java
+++ Core JAVA/StudentDAO/Main.java
@@ -4,9 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        StudentDAO dao =
-                new StudentDAO();
-
-        dao.insertStudent(5, "Rahul", 22);
+        StudentDAO dao = new StudentDAO();
+        dao.insertStudent(1, "Ravi", 41);
+        dao.insertStudent(12, "Dravid", 19);
     }
 }
